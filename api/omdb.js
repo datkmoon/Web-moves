d71de0e7e31a07ad404b4e7a99d36a137238bb30
datkmoon <waleed.raad2009@gmail.com>
2026-06-07 16:13:16 +0300

    Add OMDB API handler for movie data retrieval

---
+++ api/omdb.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  const API_KEY = '37024906';
+  const params = new URLSearchParams(req.query).toString();
+  const url = `https://www.omdbapi.com/?apikey=${API_KEY}&${params}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
